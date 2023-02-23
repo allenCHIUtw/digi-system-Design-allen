cd7c17830c1d5ce4db5f52ca0ff430b932cf7b4c
allenCHIUtw <allen@abin.com.tw>
2023-02-24 06:10:45 +0800

    2023 feb24

--- HW0/1-CR_Adder/adder.v
+++ HW0/1-CR_Adder/adder.v
@@ -2,9 +2,11 @@ module adder(x, y, carry, out);
 input [7:0] x, y;
 output carry;
 output [7:0] out;
-
+reg ans[8:0];
 /*Write your code here*/
-
+assign ans=x+y;
+out=ans[7:0];
+carry=ans[8];
 
 /*End of code*/
 
--- HW0/1-CR_Adder/adder_gate.v
+++ HW0/1-CR_Adder/adder_gate.v
@@ -2,10 +2,32 @@ module adder_gate(x, y, carry, out);
 input [7:0] x, y;
 output carry;
 output [7:0] out;
+wire [8:0]carry_out;
 
 /*Write your code here*/
+genvar  idx;
+generate
+for(idx=0; idx<8; idx=idx+1)
+ begin
+    full_adder fa(,,out[idx],);
+ end
+endgenerate
+
 
 
 /*End of code*/
 
+endmodule
+
+module full_adder(xb,yb,cin,sum,cout);
+input xb,yb,cin
+output sum ,cout;
+wire [2:0]wr;
+xor xo0(wr[0],xb,yb);
+xor xo1(sum,wr[0],cin);
+nand na0(wr[1],xb,yb);
+nand na1(wr[2],wr[0],cin);
+nand na2(cout,wr[1],wr[2]);
+
+   
 endmodule
